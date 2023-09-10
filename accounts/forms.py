08f77f3b458220f2faaa03f87ed97171119d2238
imhimansu28 <imhimansu28@gmail.com>
2023-09-10 20:39:10 +0530

    update login and registrations forms and views

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,2 +1,33 @@
+# forms.py
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
+class UserCreateForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm']
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Passwords do not match.')
+    
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
